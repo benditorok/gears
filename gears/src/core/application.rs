a6cb17222dfb53739c7debb1038c8c46d7cea485
Bendegúz Török <32034793+benditorok@users.noreply.github.com>
2024-05-17 08:43:30 +0200

    Sandbox test

--- gears/src/core/application.rs
+++ gears/src/core/application.rs
@@ -1,7 +1,16 @@
-pub trait EventLoop {}
-
-pub trait GearsApplication {
-    async fn run();
-}
+use std::{thread, time};
 
 pub struct Application {}
+
+impl Application {
+    pub fn new() -> Self {
+        Self {}
+    }
+
+    pub fn run(&self) {
+        while true {
+            println!("Running application...");
+            thread::sleep(time::Duration::from_secs(1));
+        }
+    }
+}
--- sandbox/src/main.rs
+++ sandbox/src/main.rs
@@ -1,5 +1,9 @@
+use gears::core::application;
 use gears::gears_test;
 
 fn main() {
     gears::gears_test();
+
+    let app = application::Application::new();
+    app.run();
 }
