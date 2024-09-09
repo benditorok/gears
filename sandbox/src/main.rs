08d7feb74f55b16eebcf77f158cc6ff86e631ebe
Bendegúz Török <bendi.torok@gmail.com>
2024-09-09 14:40:10 +0200

    Update sandbox main

--- sandbox/src/main.rs
+++ sandbox/src/main.rs
@@ -1,13 +1,38 @@
 use futures::executor::block_on;
 use gears::{
     core::app::{self, App},
+    ecs::{components::Position, utils::EntityBuilder, World},
     window,
 };
-use sandbox::run_sample_code;
+
+pub struct Health(i32);
+
+pub struct Name(&'static str);
 
 fn main() {
     //run_sample_code();
 
+    let mut world = World::new();
+
+    EntityBuilder::new_entity(&mut world)
+        .add_component(Name("Entity1"))
+        .add_component(Health(100))
+        .add_component(Position::new(0.0, 0.0, 0.0))
+        .build();
+
+    EntityBuilder::new_entity(&mut world)
+        .add_component(Name("Entity2"))
+        .add_component(Health(100))
+        .add_component(Position::new(1.0, 1.0, 1.0))
+        .build();
+
+    EntityBuilder::new_entity(&mut world)
+        .add_component(Name("Ent3"))
+        .add_component(Position::new(12.0, 30.0, 120.0))
+        .build();
+
     let mut app = app::GearsApp::default();
+    app.map_world(world);
+
     block_on(app.run());
 }
