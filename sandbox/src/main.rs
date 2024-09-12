ac463b44596fadc5ec2b29cc8c6045fb6ac7c10a
Bendegúz Török <bendi.torok@gmail.com>
2024-09-12 15:16:28 +0200

    Update model files

--- sandbox/src/main.rs
+++ sandbox/src/main.rs
@@ -9,82 +9,60 @@ pub struct Health(i32);
 pub struct Name(&'static str);
 
 fn main() {
-    // //run_sample_code();
-
-    // let mut world = GearsWorld::new();
-
-    // EntityBuilder::new_entity(&mut world)
-    //     .add_component(Name("Entity1"))
-    //     .add_component(Health(100))
-    //     .add_component(Position::new(0.0, 0.0, 0.0))
-    //     .build();
-
-    // EntityBuilder::new_entity(&mut world)
-    //     .add_component(Name("Entity2"))
-    //     .add_component(Health(100))
-    //     .add_component(Position::new(1.0, 1.0, 1.0))
-    //     .build();
-
-    // EntityBuilder::new_entity(&mut world)
-    //     .add_component(Name("Ent3"))
-    //     .add_component(Position::new(12.0, 30.0, 120.0))
-    //     .add_component(GearsModelData::new("res/models/cube/cube.obj"))
-    //     .build();
-
     let ecs = ecs::Manager::new();
 
+    // Cube 1
     let entity = ecs.create_entity();
-    ecs.add_component_to_entity(entity, "Hello, ECS!".to_string());
+    ecs.add_component_to_entity(entity, Name("Cube1"));
     ecs.add_component_to_entity(
         entity,
         components::GearsModelData::new("res/models/cube/cube.obj"),
     );
-    ecs.add_component_to_entity(entity, components::Position::new(1.0, 1.0, 1.0));
+    ecs.add_component_to_entity(entity, components::Position::new(10.0, 0.0, 10.0));
 
-    if let Some(component) = ecs.get_component_from_entity::<String>(entity) {
-        println!("Entity {:?} has component: {}", entity, component);
-    }
-
-    let strigns = ecs.get_all_components_of_type::<String>();
-    for (entity, component) in strigns {
-        println!("Entity {:?} has a String component: {}", entity, component);
-    }
-
-    // let entity = ecs.create_entity();
-    // ecs.add_component_to_entity(entity, "Hello, ECS!".to_string());
-    // ecs.add_component_to_entity(entity, GearsModelData::new("res/models/cube/cube.obj"));
-    // ecs.add_component_to_entity(entity, Position::new(2.0, 2.0, 2.0));
-
+    // Cube 2
     let entity = ecs.create_entity();
-    ecs.add_component_to_entity(entity, "Hello, ECS!".to_string());
+    ecs.add_component_to_entity(entity, Name("Cube2"));
     ecs.add_component_to_entity(
         entity,
         components::GearsModelData::new("res/models/cube/cube.obj"),
     );
-    ecs.add_component_to_entity(entity, components::Position::new(10.0, -10.0, 10.0));
+    ecs.add_component_to_entity(entity, components::Position::new(10.0, 0.0, -10.0));
 
+    // Cube 3
     let entity = ecs.create_entity();
-    ecs.add_component_to_entity(entity, "Hello, ECS!".to_string());
+    ecs.add_component_to_entity(entity, Name("Cube3"));
     ecs.add_component_to_entity(
         entity,
         components::GearsModelData::new("res/models/cube/cube.obj"),
     );
-    ecs.add_component_to_entity(entity, components::Position::new(5.0, -5.0, 5.0));
+    ecs.add_component_to_entity(entity, components::Position::new(-10.0, 0.0, -10.0));
 
+    // Cube 4
     let entity = ecs.create_entity();
-    ecs.add_component_to_entity(entity, "SPHERE".to_string());
+    ecs.add_component_to_entity(entity, Name("Cube4"));
     ecs.add_component_to_entity(
         entity,
-        components::GearsModelData::new("res/models/sphere/v2/sphere.obj"),
+        components::GearsModelData::new("res/models/cube/cube.obj"),
     );
-    ecs.add_component_to_entity(entity, components::Position::new(0.0, 0.0, 0.0));
+    ecs.add_component_to_entity(entity, components::Position::new(-10.0, 0.0, 10.0));
 
+    // Center sphere
+    let entity = ecs.create_entity();
+    ecs.add_component_to_entity(entity, Name("Sphere1"));
+    ecs.add_component_to_entity(
+        entity,
+        components::GearsModelData::new("res/models/sphere/sphere.obj"),
+    );
+    ecs.add_component_to_entity(entity, components::Position::new(00.0, 0.0, 0.0));
+
+    // Add random spheres
     for i in 0..=20 {
         let entity = ecs.create_entity();
         ecs.add_component_to_entity(entity, "SPHERE".to_string());
         ecs.add_component_to_entity(
             entity,
-            components::GearsModelData::new("res/models/sphere/v2/sphere.obj"),
+            components::GearsModelData::new("res/models/sphere/sphere.obj"),
         );
         // add a randdom position to them in the range of -20 to 20
         ecs.add_component_to_entity(
@@ -100,34 +78,56 @@ fn main() {
     let mut app = app::GearsApp::default();
     let ecs = app.map_ecs(ecs);
 
-    let ecs_clone = Arc::clone(&ecs);
+    // let ecs_clone = Arc::clone(&ecs);
 
-    thread::spawn(move || {
-        let mut rng = rand::thread_rng();
-        loop {
-            {
-                let ecs = ecs_clone.lock().unwrap();
+    // thread::spawn(move || {
+    //     let mut rng = rand::thread_rng();
+    //     loop {
+    //         {
+    //             let ecs = ecs_clone.lock().unwrap();
 
-                let entity = ecs.create_entity();
-                ecs.add_component_to_entity(entity, "SPHERE".to_string());
-                ecs.add_component_to_entity(
-                    entity,
-                    components::GearsModelData::new("res/models/sphere/v2/sphere.obj"),
-                );
-                // add a randdom position to them in the range of -20 to 20
-                ecs.add_component_to_entity(
-                    entity,
-                    components::Position::new(
-                        rng.gen::<f32>() * 40.0 - 20.0,
-                        rng.gen::<f32>() * 40.0 - 20.0,
-                        rng.gen::<f32>() * 40.0 - 20.0,
-                    ),
-                );
-            }
+    //             let entity = ecs.create_entity();
+    //             ecs.add_component_to_entity(entity, "SPHERE".to_string());
+    //             ecs.add_component_to_entity(
+    //                 entity,
+    //                 components::GearsModelData::new("res/models/sphere/v2/sphere.obj"),
+    //             );
+    //             // add a randdom position to them in the range of -20 to 20
+    //             ecs.add_component_to_entity(
+    //                 entity,
+    //                 components::Position::new(
+    //                     rng.gen::<f32>() * 40.0 - 20.0,
+    //                     rng.gen::<f32>() * 40.0 - 20.0,
+    //                     rng.gen::<f32>() * 40.0 - 20.0,
+    //                 ),
+    //             );
+    //         }
 
-            thread::sleep(std::time::Duration::from_secs(1));
-        }
-    });
+    //         thread::sleep(std::time::Duration::from_secs(1));
+    //     }
+    // });
 
     block_on(app.run());
 }
+
+// //run_sample_code();
+
+// let mut world = GearsWorld::new();
+
+// EntityBuilder::new_entity(&mut world)
+//     .add_component(Name("Entity1"))
+//     .add_component(Health(100))
+//     .add_component(Position::new(0.0, 0.0, 0.0))
+//     .build();
+
+// EntityBuilder::new_entity(&mut world)
+//     .add_component(Name("Entity2"))
+//     .add_component(Health(100))
+//     .add_component(Position::new(1.0, 1.0, 1.0))
+//     .build();
+
+// EntityBuilder::new_entity(&mut world)
+//     .add_component(Name("Ent3"))
+//     .add_component(Position::new(12.0, 30.0, 120.0))
+//     .add_component(GearsModelData::new("res/models/cube/cube.obj"))
+//     .build();
