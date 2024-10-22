a3aa977b1990ec5f4822a427ca30e84dadb4022d
Bendegúz Török <32034793+benditorok@users.noreply.github.com>
2024-10-22 12:37:02 +0200

    Create a minimal example

--- examples/src/minimal.rs
+++ examples/src/minimal.rs
@@ -1,3 +1,36 @@
-fn main() {
-    println!("Hello, world!");
+use app::GearsApp;
+use cgmath::Rotation3;
+use ecs::traits::EntityBuilder;
+use gears::prelude::*;
+use log::{self, info};
+use std::f32::consts::PI;
+use std::sync::Arc;
+use std::{env, thread};
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let mut app = GearsApp::default();
+
+    app.new_entity() // Add fixed camera
+        .add_component(components::Name("Fixed Camera"))
+        .add_component(components::Pos3::new(cgmath::Vector3::new(
+            20.0, 15.0, 20.0,
+        )))
+        .add_component(components::Camera::Fixed {
+            look_at: components::Pos3::new(cgmath::Vector3::new(0.0, 10.0, 0.0)),
+        })
+        .new_entity() // Add ambient light
+        .add_component(components::Name("Ambient Light"))
+        .add_component(components::Light::Ambient)
+        .add_component(components::Pos3::new(cgmath::Vector3::new(0.0, 50.0, 0.0)))
+        .new_entity() // Add a sphere
+        .add_component(components::Name("Sphere1"))
+        .add_component(components::Model::Dynamic {
+            obj_path: "res/models/sphere/sphere.obj",
+        })
+        .add_component(components::Pos3::new(cgmath::Vector3::new(0.0, 0.0, 0.0)))
+        .build();
+
+    app.run().await?;
+    Ok(())
 }
