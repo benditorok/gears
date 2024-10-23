d1e9a4d02345c559497f80bc4561b93549be2aae
Bendegúz Török <32034793+benditorok@users.noreply.github.com>
2024-10-23 12:09:02 +0200

    Add default add_components fn to EntityBuilder

--- gears/src/ecs/traits.rs
+++ gears/src/ecs/traits.rs
@@ -3,5 +3,15 @@ use super::Entity;
 pub trait EntityBuilder {
     fn new_entity(&mut self) -> &mut Self;
     fn add_component<T: 'static + Send + Sync>(&mut self, component: T) -> &mut Self;
+    fn add_components<T: 'static + Send + Sync, I>(&mut self, components: I) -> &mut Self
+    where
+        I: IntoIterator<Item = T>,
+    {
+        for component in components {
+            self.add_component(component);
+        }
+
+        self
+    }
     fn build(&mut self) -> Entity;
 }
