9707fd7d20b969f2af31d1816a5f53e2c4d88990
Bendegúz Török <32034793+benditorok@users.noreply.github.com>
2024-10-28 10:16:25 +0100

    Update custom windows example

--- examples/src/custom_window.rs
+++ examples/src/custom_window.rs
@@ -1,22 +1,9 @@
-use cgmath::{One, Quaternion, Rotation3};
-use gears::{new_entity, prelude::*};
-use log::{warn, LevelFilter};
+use std::sync::mpsc;
 
-pub fn example_gui(ui: &egui::Context) {
-    egui::Window::new("TestWindow")
-        .default_open(true)
-        .max_width(1000.0)
-        .max_height(800.0)
-        .default_width(800.0)
-        .resizable(true)
-        .default_pos([0.0, 0.0])
-        .show(ui, |ui| {
-            if ui.add(egui::Button::new("Click me")).clicked() {
-                warn!("Button clicked in the custom window!");
-            }
-            ui.end_row();
-        });
-}
+use cgmath::{Euler, One, Quaternion, Rad, Rotation3};
+use egui::{Align, Align2};
+use gears::{core::Dt, new_entity, prelude::*};
+use log::{warn, LevelFilter};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -28,25 +15,7 @@ async fn main() -> anyhow::Result<()> {
 
     let mut app = GearsApp::default();
 
-    // Add the custom window
-    app.add_window(Box::new(example_gui));
-
-    app.add_window(Box::new(move |ui| {
-        egui::Window::new("Window")
-            .default_open(true)
-            .max_width(1000.0)
-            .max_height(800.0)
-            .default_width(800.0)
-            .resizable(true)
-            .default_pos([400.0, 0.0])
-            .show(ui, |ui| {
-                if ui.add(egui::Button::new("Click me")).clicked() {
-                    warn!("Button clicked in the custom window!");
-                }
-                ui.end_row();
-            });
-    }));
-
+    // ! Entities
     // Add fixed camera
     new_entity!(
         app,
@@ -98,8 +67,92 @@ async fn main() -> anyhow::Result<()> {
         },
     );
 
+    // ! Custom windows
+    let (dt_tx, dt_rx) = tokio::sync::mpsc::channel::<Dt>(10);
+
+    // Informations about the renderer
+    let (w1_frame_tx, w1_frame_rx) = mpsc::channel::<Dt>();
+    app.add_window(Box::new(move |ui| {
+        egui::Window::new("Renderer info")
+            .default_open(true)
+            .max_width(1000.0)
+            .max_height(800.0)
+            .default_width(800.0)
+            .resizable(true)
+            .anchor(Align2::RIGHT_TOP, [0.0, 0.0])
+            .show(ui, |ui| {
+                if let Ok(dt) = w1_frame_rx.try_recv() {
+                    ui.label(format!("Frame time: {:.2} ms", dt.as_secs_f32() * 1000.0));
+                    ui.label(format!("FPS: {:.0}", 1.0 / dt.as_secs_f32()));
+                }
+                ui.end_row();
+            });
+    }));
+
+    // Move the object around
+    let cw_ecs = app.get_ecs();
+    app.add_window(Box::new(move |ui| {
+        egui::Window::new("Sphere")
+            .default_open(true)
+            .max_width(1000.0)
+            .max_height(800.0)
+            .default_width(800.0)
+            .resizable(true)
+            .anchor(Align2::LEFT_TOP, [0.0, 0.0])
+            .show(ui, |ui| {
+                if let Some(sphere) = cw_ecs
+                    .lock()
+                    .unwrap()
+                    .get_component_from_entity::<components::model::StaticModel>(sphere_entity)
+                {
+                    let mut wlock_sphere = sphere.write().unwrap();
+                    ui.label("Position");
+                    ui.add(egui::Slider::new(
+                        &mut wlock_sphere.position.x,
+                        -10.0..=10.0,
+                    ));
+                    ui.add(egui::Slider::new(
+                        &mut wlock_sphere.position.y,
+                        -10.0..=10.0,
+                    ));
+                    ui.add(egui::Slider::new(
+                        &mut wlock_sphere.position.z,
+                        -10.0..=10.0,
+                    ));
+                    ui.label("Rotation");
+                    let euler = Euler::from(wlock_sphere.rotation);
+                    let mut pitch = euler.x.0;
+                    let mut yaw = euler.y.0;
+                    let mut roll = euler.z.0;
+
+                    ui.add(
+                        egui::Slider::new(&mut pitch, -std::f32::consts::PI..=std::f32::consts::PI)
+                            .text("Pitch"),
+                    );
+                    ui.add(
+                        egui::Slider::new(&mut yaw, -std::f32::consts::PI..=std::f32::consts::PI)
+                            .text("Yaw"),
+                    );
+                    ui.add(
+                        egui::Slider::new(&mut roll, -std::f32::consts::PI..=std::f32::consts::PI)
+                            .text("Roll"),
+                    );
+
+                    wlock_sphere.rotation = Quaternion::from(Euler {
+                        x: Rad(pitch),
+                        y: Rad(yaw),
+                        z: Rad(roll),
+                    });
+                }
+                ui.end_row();
+            });
+    }));
+
     // Use the update loop to spin the sphere
     app.update_loop(move |ecs, dt| {
+        // Send the frame time to the custom window
+        w1_frame_tx.send(dt).unwrap();
+
         let ecs = ecs.lock().unwrap();
         let spin_speed = 0.5f32;
 
