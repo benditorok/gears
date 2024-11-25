95d03826c1f4bc42822defd3fe18339fa1633a64
Bendegúz Török <32034793+benditorok@users.noreply.github.com>
2024-11-25 14:30:44 +0100

    Update build script

--- gears/build.rs
+++ gears/build.rs
@@ -2,27 +2,33 @@ use anyhow::*;
 use fs_extra::copy_items;
 use fs_extra::dir::CopyOptions;
 use std::env;
+use std::path::PathBuf;
 
-fn main() -> anyhow::Result<()> {
-    // Environment setup
-    let exec_dir = env::current_dir()?;
-    let res_dir_val = exec_dir.join("../res");
-    println!(format!("cargo:rustc-env=RES_DIR={}", res_dir_val.display()));
+fn main() -> Result<()> {
+    // Get output directory where executable will be built
+    let out_dir = PathBuf::from(env::var("OUT_DIR")?);
+    // Go up 3 levels to get to target/debug or target/release
+    let target_dir = out_dir.ancestors().nth(3).unwrap();
 
-    // This tells cargo to rerun this script if something in res/ changes.
-    println!("cargo:rerun-if-changed=../res/*");
+    // Watch for resource changes
+    println!("cargo:rerun-if-changed=res/*");
 
-    // Prepare what to copy and how
+    // Setup copy options
     let mut copy_options = CopyOptions::new();
     copy_options.overwrite = true;
-    let paths_to_copy = vec!["../res/"];
 
-    // Copy them next to the compiled executable and set an environment variable for it
-    let res_dir = env::current_dir()?.join("../res");
+    // Copy resources relative to executable
+    let res_source = env::current_dir()?.join("../res");
+    let res_target = target_dir.join("res");
 
-    // Copy the items to the directory where the executable will be built
-    let res_dir = env::var("RES_DIR")?;
-    copy_items(&paths_to_copy, res_dir, &copy_options)?;
+    // Create target directory if it doesn't exist
+    std::fs::create_dir_all(&res_target)?;
+
+    // Copy resource directory
+    copy_items(&[res_source], target_dir, &copy_options)?;
+
+    // Set environment variable for resource path
+    println!("cargo:rustc-env=RES_DIR=res");
 
     Ok(())
 }
