c16a31b2d27cb4438a7a0bf206c99a336f958479
Bendegúz Török <32034793+benditorok@users.noreply.github.com>
2025-11-24 20:01:36 +0100

    Update clear color in base pipeline

--- crates/gears-renderer/src/state/pipeline/base.rs
+++ crates/gears-renderer/src/state/pipeline/base.rs
@@ -277,9 +277,9 @@ impl BasePipeline {
                 resolve_target: None,
                 ops: wgpu::Operations {
                     load: wgpu::LoadOp::Clear(wgpu::Color {
-                        r: 0.1,
-                        g: 0.2,
-                        b: 0.3,
+                        r: 0.53,
+                        g: 0.81,
+                        b: 0.92,
                         a: 1.0,
                     }),
                     store: wgpu::StoreOp::Store,
